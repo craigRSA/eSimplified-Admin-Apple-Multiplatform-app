05087eca0d9e743b4ca2155f2199bd6017ab1eed
Craig <craig@perkel.co.za>
2026-06-18 08:08:33 -0400

    feat: add iOS 17 to EsimPulseKit supported platforms

--- EsimPulseKit/Package.swift
+++ EsimPulseKit/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "EsimPulseKit",
-    platforms: [.macOS(.v14)],
+    platforms: [.macOS(.v14), .iOS(.v17)],
     products: [
         .library(name: "EsimPulseKit", targets: ["EsimPulseKit"]),
     ],
